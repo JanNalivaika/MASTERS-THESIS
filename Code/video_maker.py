4b8b3b9320a4b9998c48a4cce71de5bb9ed7cb27
Jan <nalivaika@outlook.de>
2024-02-20 10:51:29 +0100

    v2 der abschluss powerpoint mit merh GIF

--- Code/video_maker.py
+++ Code/video_maker.py
@@ -6,12 +6,12 @@ from natsort import natsorted, ns
 def make_gif(frame_folder):
 
     elems = glob.glob(f"{frame_folder}/*.png")
-    elems = natsorted(elems, key=lambda y: y.lower())
+    elems = natsorted(elems, key=lambda y: y.lower())#
     frames = [Image.open(image) for image in elems]
     frame_one = frames[0]
-    frame_one.save("my_awesome_3.gif", format="GIF", append_images=frames,
-                   save_all=True, duration=200, loop=0)
+    frame_one.save("my_awesome_5.gif", format="GIF", append_images=frames,
+                   save_all=True, duration=80, loop=0)
 
 
 if __name__ == "__main__":
-    make_gif("../Latex/figures/GIF")
+    make_gif("robot_tilt")
